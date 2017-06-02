475f768a632252c5c2cb3dacc3fb22a02953496a
James Peace III <japeace2020@mirmanmustangs.org>
2017-06-02 14:38:58 -0700

    Working 4

--- 1-git-down-with-markdown/index.md
+++ 1-git-down-with-markdown/index.md
@@ -4,6 +4,17 @@ James was born on June 27, 2005 in Los Angeles, California. He is currently 11 y
 
 ---
 ## Hobbies
-James started playing
+
+### Video Games
+
+James likes video games and his favorite ones are Splatoon, Splatoon 2, and Paper Mario Color Splash.
+
+(http://assets1.ignimgs.com/thumbs/userUploaded/2017/1/13/splatoon221280-1484322406992_large.jpg)
+
+### Sports
+
+#### Water Polo
+
+James started playing water polo in September of 2015.
 
 ---
--- 4-rps/app.js
+++ 4-rps/app.js
@@ -1,1 +1,35 @@
-console.log("I'm working!");
+var beats = {
+    "rock":"scissors",
+    "paper":"rock",
+    "scissors":"paper"
+}
+
+// var message = `rock beats ${beats["rock"]}`;
+
+// document.write(message);
+
+// Get computer random choice
+function getComputerChoice(){
+    var choices = Object.keys(beats);
+    var choice = choices[Math.floor(Math.random()* 3)];
+    console.log(choice);
+    return choice;
+}
+
+function getPlayerChoice(c) {
+    var computerChoice = getComputerChoice ();
+    if (beats[c]) == computerChoice) {
+        alert(`You rekt ${computerChoice}`);
+    } else if (beats[computerChoice] == c) {
+        alert(`You got rekt by ${computerChoice}`);
+    } else {
+        alert("Tie!");
+    }
+
+}
+    }
+}
+
+// Determine winner
+
+// Output who won/modify scoreboard
